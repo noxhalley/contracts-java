23c7926128415b618ba8cb209eebfd61c1948be4
Dang Hai <pecheule34713@gmail.com>
2025-08-05 00:31:35 +0700

    update script

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -e
 
+VERSION_CLEANED="${VERSION#v}"
+sed -i "0,/<version>.*<\/version>/s//<version>${VERSION_CLEANED}<\/version>/" pom.xml
+
 git config --global user.name "${GIT_USER_NAME}"
 git config --global user.email "${GIT_USER_EMAIL}"
 
 git add src/main/java
+git add pom.xml
 git diff --cached --quiet || git commit -m "Update from ${VERSION}"
 
 git tag -a $VERSION -m "Version ${VERSION}"
